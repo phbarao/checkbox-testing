d219576501769db98eb08d645ed25ba4ec34833f
Pedro Barão <phbarao@gmail.com>
2021-08-24 18:13:05 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CheckBoxWithLabel from "./components/CheckBoxWithLabel/index.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Testing React</h1>
+      <CheckBoxWithLabel />
+    </div>
+  );
+}
+
+export default App;
